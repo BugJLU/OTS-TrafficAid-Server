e0a5c8a03860dc5b21d6d86ca154ee511ad92310
activemy <activemayue@gmail.com>
2018-04-29 07:59:13 +0800

    json OK

--- src/main/java/org/bugjlu/ots_server/web/controller/test.java
+++ src/main/java/org/bugjlu/ots_server/web/controller/test.java
@@ -1,6 +1,7 @@
 package org.bugjlu.ots_server.web.controller;
 
 import org.bugjlu.ots_server.po.User;
+import org.bugjlu.ots_server.web.vo.testvo;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,15 +10,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @Controller
 public class test {
-    @RequestMapping(value = "login", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public User login(HttpServletRequest request) {
+    @RequestMapping("/login")
+    //@RequestMapping(value = "login", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody List<testvo> login(HttpServletRequest request) {
         System.out.println(request.getParameter("a"));
-        User result = new User();
-        result.setId("111111");
-        return result;
+        testvo result1 = new testvo();
+        testvo result2 = new testvo();
+        result1.setId("111111");
+        result2.setId("222222");
+        List<testvo> lv = new ArrayList<>();
+        lv.add(result1);
+        lv.add(result2);
+        return lv;
     }
 }
